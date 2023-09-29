7772c7867b652331cd1eb651926bddd3c63af761
Abdalrahman(0x80) <78023331+abdalrahman0x80@users.noreply.github.com>
2023-09-29 11:15:41 +0300

    Add files via upload
    upload file 9/29/2023

---
+++ app/app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using System.Collections.Generic;
+/* get processes and id process */
+Process[] ps = Process.GetProcesses();
+foreach(Process process in ps)
+{
+    Console.WriteLine($"App Process Name: [{process.ProcessName}] and pid: [{process.Id}]");
+}
+Console.WriteLine("------------------------------------");
+
+Console.WriteLine("Press Enter.....");
+Console.ReadKey();
